cfd524fe48b5c3ad845391b848d77930dca7f3ab
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2023-02-28 20:36:49 -0300

    simple login

--- app/src/main/java/com/example/thermoledmobileclient/LoginActivity.java
+++ app/src/main/java/com/example/thermoledmobileclient/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.thermoledmobileclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /** Called when the user taps the Home button */
+    public void toLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
